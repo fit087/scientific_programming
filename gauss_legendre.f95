468c30afd45e015d9d80517d6b334b1714bbcdaa
Adolfo Correa <fitomillennium@hotmail.com>
2016-05-04 23:54:25 -0300

    newton raphson function customized
    newton raphson customized for this problem. (Legendre polynomial roots)

--- gauss_legendre.f95
+++ gauss_legendre.f95
@@ -1,11 +1,70 @@
-program gauss_legendre
+!
+!	*****************************************************************
+!     Gauss-Legendre - Program of Numeric Integration
+!     
+!
+!       Adolfo Correa <fitomillennium@hotmail.com> 04/05/2016
+!     *****************************************************************
+!module ExampleFuncs
 
+!   implicit none
+
+!contains
+
+!function newton_raphson (fun)
+!  real :: newton_raphson
+!  
+!  abstract interface
+!      function func (z)
+!         real :: func
+!         real, intent (in) :: z
+!      end function func
+!   end interface
+  
+!  procedure (func), pointer :: fun
+
+!  newton_raphson = fun (5.)
+
+!  return
+!end function newton_raphson
+
+!end module ExampleFuncs
+
+
+module interfaz
+	interface
+
+		function fx(x,n)
+			real*8:: fx, x
+			integer::n      
+		end function fx
+
+		function fprime(x,n)
+      		real*8:: fx, x
+      		integer::n  
+		end function fprime
+
+	end interface
+end module interfaz
+
+program gauss_legendre
+!	use interfaz
       implicit none
+!      use ExampleFuncs
       
       integer i, n, j
-      real*8 x0, x, legendre_pol2
+      real*8 x0, x, legendre_pol2, fprime, newton_raphson
       
       real*8, parameter :: pi = 4.0 * atan(1.0)
+      
+!   abstract interface
+!      function func (z)
+!         real :: func
+!         real, intent (in) :: z
+!      end function func
+!   end interface
+
+!	procedure (fx), pointer :: f_ptr => null ()
 
       print *, '**************'      
       print *, 'Gauss-Legendre'
@@ -17,18 +76,57 @@ program gauss_legendre
             
                   x0 = cos(pi*(i-0.25)/(n+0.5))
                   x = x0
-                  do j = 1, 3, 1
+                  !do j = 1, 3, 1
 !                        x = x - legendre_pol2(x,n)
-                  print *, 'legendre_pol2(x,n) = ',  legendre_pol2(x,n)
-                  end do
-                  print *, i, x0
+                  !print *, 'legendre_pol2(x,n) = ',  legendre_pol2(x,n)
+                  !print *, 'fprime(x,n) = ',  fprime(x,n)
+                  !end do
+!                  print *, i, x0
+                  print *, i, newton_raphson(x0, n)
       
             end do
       end do
 
 end program gauss_legendre
 
-recursive real*8 function legendre_pol2(x,n) result(legendre_pol)
+!real*8 pure function newton_raphson(x0, fx, fprime) result (x)
+!	implicit none
+!	real*8, intent(in) 	::x0
+!	real*8			::x=x0
+!	
+!	do
+!		x = x - fx(x) / fprime(x)
+!		if (fx(x)/fprime(x)> 2*tiny(x)) exit
+!	enddo
+!
+!end function newton_raphson
+
+real*8 function newton_raphson(x0, n) result (x)
+	implicit none
+	real*8, intent(in) 		:: x0
+!	real*8				:: x=x0
+	real*8 legendre_pol2, fprime
+	integer, intent(in)		:: n
+	
+	x=x0
+	do
+		x = x - legendre_pol2(x,n) / fprime(x,n)
+		if (legendre_pol2(x,n)/fprime(x,n)> 2*tiny(x)) exit
+	enddo
+
+end function newton_raphson
+
+recursive real*8 function fprime(x,n) result(p_prime)
+	implicit none
+	real*8, intent(in) 	:: x
+	integer, intent(in) 	:: n
+	real*8			:: legendre_pol2
+	
+	p_prime = n/(x**2-1)*(x*legendre_pol2(x,n)-legendre_pol2(x,n-1))
+
+end function fprime
+
+recursive real*8 pure function legendre_pol2(x,n) result(legendre_pol)
 
       implicit none
 
