74edee4171633b70da206bcc95caefa496ee585d
Adolfo Correa <fitomillennium@hotmail.com>
2016-05-31 00:11:37 -0300

    testing data structure
    testing data structure with dynamic allocation

---
+++ datadyn.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <new>			// new and delete
+#include <vector>
+
+#include <cmath>
+
+using namespace std;
+//std::bad_alloc;
+
+struct pts_gauss
+{
+	int num;
+	/*double x[num]; don't work
+	double w[num];*/
+	double *x = new double[num];
+	double *w = new double[num];
+};
+
+typedef struct
+{
+	int num;
+	/*double x[num]; don't work
+	double w[num];*/
+	double *x = new double[num];
+	double *w = new double[num];
+}pi_gauss;
+
+void prints(struct pts_gauss ptos);
+
+int main(int argc, char* argv[])
+{
+	// Reader
+	
+	struct pts_gauss pts;
+	//vector<int> xis(6,0);
+	
+	pts.num = 10;
+	//xis.reserve(6);
+	
+	for(int i = 1; i<=10;i++)
+	{
+		pts.x[i-1] = pow(i,2)*sqrt(i);
+		pts.w[i-1] = i*sqrt(i);
+		//xis[i-1] = sqrt(i);
+		//xis.push_back(sqrt(i));
+		//cout<<xis[i];
+	}
+	
+	prints(pts);
+	
+	
+	//delete[] pts;
+}
+
+void prints(struct pts_gauss ptos)
+{
+	for(int i=0; i<ptos.num; i++)
+		cout<<"x"<<i<<"= "<<ptos.x[i]<<"\t\tw"<<i<<"= " \
+		<<ptos.w[i]<<endl;
+}
