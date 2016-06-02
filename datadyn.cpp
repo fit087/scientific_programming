846b75fdcad5eca13485247e186f3874f80351db
Adolfo Correa <fitomillennium@hotmail.com>
2016-06-02 12:55:12 -0300

    Using new typedef It was used the new typedef and it was add a new function to print that new type.

--- datadyn.cpp
+++ datadyn.cpp
@@ -26,21 +26,28 @@ typedef struct
 }pi_gauss;
 
 void prints(struct pts_gauss ptos);
+void prints1(pi_gauss ptos);
 
 int main(int argc, char* argv[])
 {
 	// Reader
 	
 	struct pts_gauss pts;
+	pi_gauss puntos;
 	//vector<int> xis(6,0);
 	
 	pts.num = 10;
+	puntos.num = 10;
 	//xis.reserve(6);
 	
 	for(int i = 1; i<=10;i++)
 	{
-		pts.x[i-1] = pow(i,2)*sqrt(i);
-		pts.w[i-1] = i*sqrt(i);
+		/*pts.x[i-1] = pow(i,2)*sqrt(i);
+		pts.w[i-1] = i*sqrt(i);*/
+		/*puntos.x[i-1] = pow(i,2)*sqrt(i*i*i);
+		puntos.w[i-1] = i*sqrt(i*i*i);*/
+		*(puntos.x+i-1) = pow(i,2)*sqrt(i*i*i);
+		*(puntos.w+i-1) = i*sqrt(i*i*i);
 		//xis[i-1] = sqrt(i);
 		//xis.push_back(sqrt(i));
 		//cout<<xis[i];
@@ -56,5 +63,12 @@ void prints(struct pts_gauss ptos)
 {
 	for(int i=0; i<ptos.num; i++)
 		cout<<"x"<<i<<"= "<<ptos.x[i]<<"\t\tw"<<i<<"= " \
-		<<ptos.w[i]<<endl;
+		<<ptos.w[i]<<endl;	
+}
+
+void prints1(pi_gauss ptos)
+{
+	for(int i=0; i<ptos.num; i++)
+		cout<<"x"<<i<<"= "<<ptos.x[i]<<"\t\tw"<<i<<"= " \
+		<<ptos.w[i]<<endl;	
 }
